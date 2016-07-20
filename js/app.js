817b00704c6c3e4f74341db619b2423be16d9a66
Daniel <ortego88@gmail.com>
2016-07-20 21:46:28 +0200

    map and marker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
-var app = angular.module('scoutingApp', [])
+var app = angular.module('scoutingApp', ['ngMap'])
 
 app.controller("teams", ["$scope","$http", function($scope, $http){
 	$http.get("js/footballJson.json").success(function(data){
 		$scope.teams = data;
 	});
+
+	if (navigator.geolocation) navigator.geolocation.getCurrentPosition(onPositionUpdate);
+ 
+function onPositionUpdate(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true";
+    $http.get(url)
+        .then(function(result) {
+            var address = result.data.results[0].formatted_address;
+            var lat1 = result.data.results[0].geometry.location.lat;
+            var lon1 = result.data.results[0].geometry.location.lng;
+            $scope.address = address;
+            $scope.lat1 = lat1;
+            $scope.lon1 = lon1;
+            console.log(position)
+            console.log(position.coords.longitude)
+
+        });
+};
+
+$scope.$on('mapInitialized', function(event, map) {
+    $scope.map = map;
+  });
+
+
 }]);
+
+
+
